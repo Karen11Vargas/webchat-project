b01f39ffa8e3c2143a1f0e74b7c00748008ceb37
Karen Vargas <kalivargas.11@gmail.com>
2024-11-26 19:54:00 -0500

    Agregar mejores estilos

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,41 +1,61 @@
 const socket = io();
 
-const send = document.querySelector("#send-message");
-const allMessages = document.querySelector("#all-messages");
+const send = get(".msger-send-btn");
+const msgerChat = get(".msger-chat");
+
+// Función para obtener una cookie por su nombre
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+      return parts.pop().split(';').shift();
+  }
+  return null; 
+}
+
+const username = getCookie('username');
+
 
 send.addEventListener("click", () => {
 
-    const message = document.querySelector("#message");
-
-    socket.emit("send-message", message.value);
+  const message = get(".msger-input");
+  if (message.value != "") {
+    socket.emit("send-message",message.value);
     message.value=""
-
-
+  }
 });
 
 socket.on("message", ({user, message}) => {
 
-    const msg = document.createRange().createContextualFragment(`
-    <div class="message">
-                
-        <div class="image-container">
-            <img src="/img/img.png">
+    const isOwnMessage = user === username;
+
+    const msg = `
+    <div class="msg ${isOwnMessage ? 'right-msg' : 'left-msg'}">
+      <div class="msg-img" style="background-image: url(https://xsgames.co/randomusers/avatar.php?g=pixel)"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${user}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
         </div>
 
-        <div class="message-body">
-            
-            <div class="user-info">
-                <span class="username">${user}</span>
-                <span class="time">Hace 1 segundo</span>
-            </div>
-
-            <p>${message}</p>
-
-        </div>
-        
+        <div class="msg-text">${message}</div>
+      </div>
     </div>
-    `);
+    `;
 
-    allMessages.append(msg);
-
+    msgerChat.insertAdjacentHTML("beforeend", msg);
+    msgerChat.scrollTop += 500;
 });
+
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
